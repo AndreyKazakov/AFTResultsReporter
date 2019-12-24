276e9e9f04dd952fbea568c38034daa5ca59a652
AndreyKazakov <33053969+AndreyKazakov@users.noreply.github.com>
2019-12-24 16:05:36 +0300

    temp updates

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import microsoft.exchange.webservices.data.core.ExchangeService;
+import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
+import microsoft.exchange.webservices.data.core.enumeration.property.BodyType;
+import microsoft.exchange.webservices.data.core.service.item.EmailMessage;
+import microsoft.exchange.webservices.data.credential.ExchangeCredentials;
+import microsoft.exchange.webservices.data.credential.WebCredentials;
+import microsoft.exchange.webservices.data.property.complex.EmailAddress;
+import microsoft.exchange.webservices.data.property.complex.MessageBody;
+
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class Main {
 
@@ -12,6 +23,30 @@ public class Main {
             writer.write(ResultsWriter.getHtmlReport());
             writer.flush();
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        ExchangeService service = new ExchangeService();
+        ExchangeCredentials credentials = new WebCredentials("", "", "");
+        service.setCredentials(credentials);
+
+        try {
+            service.setUrl(new URI("https://webmail.tele2.ru/EWS/Exchange.asmx"));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            EmailMessage msg = new EmailMessage(service);
+            msg.setSubject("Hello world!");
+            String bodyText = "<b>test message</b>";
+            msg.setBody(new MessageBody(BodyType.HTML, bodyText));
+            msg.getToRecipients().add(new EmailAddress("andrey.kazakov@aplana.com"));
+           // msg.setFrom(new EmailAddress("andrey.kazakov@aplana.com"));
+            msg.send();
+        } catch (Exception e){
             e.printStackTrace();
         }
 
